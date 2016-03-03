732e751322ddd47f48d0f20e1a6bae5585834270
binlyzhuo <binlyzhuo@outlook.com>
2016-03-03 21:05:38 +0800

    ~commit codes

---
+++ LearnCSharp/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnCSharp
+{
+    public class Product
+    {
+        private readonly string name;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        private readonly decimal price;
+
+        public decimal Price
+        {
+            get { return price; }
+        }
+
+        public Product(string name, decimal price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public static List<Product> GetSampleProduct()
+        {
+            var list = new List<Product>();
+            list.Add(new Product("book", 100));
+            return list;
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return string.Format("{0}:{1}",name,price);
+        }
+    }
+}
---
+++ LearnCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Just Do it!!");
+            Console.ReadLine();
+        }
+    }
+}
