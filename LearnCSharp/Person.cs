0d040536454aafee1049066866c21fc2eb9c8ed9
binlyzhuo <binlyzhuo@outlook.com>
2016-03-08 21:55:45 +0800

    ~commit codes

---
+++ LearnCSharp/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace LearnCSharp
+{
+    public class Person
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        private static int InstaceCount { get; set; }
+
+        private static readonly object counterLock = new object();
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+
+            lock (counterLock)
+            {
+                InstaceCount++;
+            }
+        }
+    }
+}
