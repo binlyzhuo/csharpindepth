5b59f6bd6582d0e791a5ae3574901adc23f67f0c
binlyzhuo <binlyzhuo@outlook.com>
2017-01-03 21:28:21 +0800

    ~

---
+++ Chpt1/ProductNameComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chpt1
+{
+    public class ProductNameComparer:IComparer<Product>
+    {
+        public int Compare(Product x, Product y)
+        {
+            return x.Name.CompareTo(y.Name);
+            
+        }
+    }
+}
--- Chpt1/Program.cs
+++ Chpt1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,54 @@ namespace Chpt1
     {
         static void Main(string[] args)
         {
+            
+            Search();
+            Console.ReadLine();
+        }
 
+        static void Sort()
+        {
+            var products = Product.GetSampleProducts();
+            products.Sort(new ProductNameComparer());
+
+            products.Sort(delegate (Product x, Product y)
+            {
+                return x.Name.CompareTo(y.Name);
+            });
+
+            products.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+            foreach (var product in products.OrderBy(p => p.Name))
+            {
+                Console.WriteLine(product);
+            }
+
+            Console.WriteLine("------------------");
+
+            foreach (var p in products)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        static void Search()
+        {
+            List<Product> products = Product.GetSampleProducts();
+            Predicate<Product> test = delegate(Product p)
+            {
+                return p.Price > 10m;
+            };
+
+            var mathes = products.FindAll(test);
+            Action<Product> print = Console.WriteLine;
+            mathes.ForEach(print);
+
+            products.FindAll(delegate(Product p) { return p.Price > 10m; }).ForEach(print);
+
+            foreach (var product in products.Where(p=>p.Price>10m))
+            {
+                Console.WriteLine(product);
+            }
         }
     }
 }
