83996f834488a90282241326bb4f2147ace0a1b4
binlyzhuo <binlyzhuo@outlook.com>
2017-01-04 20:25:56 +0800

    ~

---
+++ Chpt3/RefSample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chpt3
+{
+    public struct RefSample<T>
+        where T:class 
+    {
+    }
+
+    class ValSample<T>
+        where T : struct
+    {
+        
+    }
+}
