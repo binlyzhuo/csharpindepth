fa787c10a51022d2a5a5a748a90062755344a287
binlyzhuo <binlyzhuo@outlook.com>
2017-01-05 20:26:08 +0800

    ~

---
+++ Chpt5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chpt5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void LoadPlainEvent(object sender, EventArgs e)
+        {
+            Console.WriteLine("LogPlain");
+        }
+    }
+}
