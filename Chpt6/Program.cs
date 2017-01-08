e09adbb70f821c85a11f9c28fc8e37ae3a12629c
binlyzhuo <binlyzhuo@outlook.com>
2017-01-08 18:35:26 +0800

    !t

--- Chpt6/Program.cs
+++ Chpt6/Program.cs
@@ -10,6 +10,18 @@ namespace Chpt6
     {
         static void Main(string[] args)
         {
+            Console.ReadLine();
         }
+
+        static readonly string Padding = new string(' ',30);
+        static IEnumerable<int> CreateEnumerable()
+        {
+            Console.WriteLine("{0} Start Of CreateEnumerable()",Padding);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("{0} About to yield {1}",Padding,1);
+                yield return i;
+            }
+        } 
     }
 }
