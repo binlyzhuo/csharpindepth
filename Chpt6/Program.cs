3a332e43fa9a07703cedd89b6ab9b5566c9547f8
binlyzhuo <binlyzhuo@outlook.com>
2017-01-08 18:51:19 +0800

    ~commit

--- Chpt6/Program.cs
+++ Chpt6/Program.cs
@@ -10,6 +10,21 @@ namespace Chpt6
     {
         static void Main(string[] args)
         {
+            IEnumerable<int> iterable = CreateEnumerable();
+            IEnumerator<int> iterator = iterable.GetEnumerator();
+            Console.WriteLine("Start to iterate");
+            while (true)
+            {
+                Console.WriteLine("Calling MoveNext()...");
+                bool result = iterator.MoveNext();
+                Console.WriteLine("...MoveNext result:{0}",result);
+                if (!result)
+                {
+                    break;
+                }
+                Console.WriteLine("Fetching Current...");
+                Console.WriteLine("..Current result:{0}",iterator.Current);
+            }
             Console.ReadLine();
         }
 
@@ -21,7 +36,11 @@ namespace Chpt6
             {
                 Console.WriteLine("{0} About to yield {1}",Padding,1);
                 yield return i;
+                Console.WriteLine("{0} After yield!",Padding);
             }
+
+            yield return -1;
+            Console.WriteLine("{0} End of CreateEnumerable()",Padding);
         } 
     }
 }
