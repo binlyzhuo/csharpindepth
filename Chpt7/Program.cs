111c52600bdfca07d778b480c4777900899dd918
binlyzhuo <binlyzhuo@outlook.com>
2017-01-09 21:11:27 +0800

    pragma

--- Chpt7/Program.cs
+++ Chpt7/Program.cs
@@ -11,6 +11,10 @@ namespace Chpt7
         static void Main(string[] args)
         {
             PartialMethodDemo m = new PartialMethodDemo();
+
+#pragma warning disable 0219
+            int num = 10;
+#pragma warning restore 0219
             Console.ReadLine();
         }
     }
