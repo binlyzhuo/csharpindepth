cb3dcdfa7a189c623db5af0d8521db9c9a6460aa
binlyzhuo <binlyzhuo@outlook.com>
2017-01-11 20:57:57 +0800

    linq

--- Chpt11/Program.cs
+++ Chpt11/Program.cs
@@ -10,7 +10,13 @@ namespace Chpt11
     {
         static void Main(string[] args)
         {
+            var adultNames = from p in Person.Samples()
+                where p.Age >= 18
+                select p.Name;
 
+            Console.WriteLine(adultNames.Count());
+
+            Console.ReadLine();
         }
     }
 }
