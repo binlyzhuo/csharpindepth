4b93372f875585381802dc83a940f6a49b2d2d19
binlyzhuo <binlyzhuo@outlook.com>
2017-01-11 21:40:49 +0800

    chpt11

---
+++ Chpt11/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chpt11
+{
+    public class Person
+    {
+        public string Name { set; get; }
+        public int Age { set; get; }
+
+        public static List<Person> Samples()
+        {
+            return new List<Person>()
+            {
+                new Person() {Name = "Bin",Age = 20}
+            };
+        } 
+    }
+}
---
+++ Chpt11/SampleData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chpt11
+{
+    public class SampleData
+    {
+        public List<User> AddUsers()
+        {
+            return new List<User>()
+            {
+               new User() {Name = "Bin"}
+            };
+        } 
+    }
+
+    public class User
+    {
+        public string Name { set; get; }
+    }
+}
