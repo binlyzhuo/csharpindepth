42f10747dfd498959e77e55d01c47af3a63a44ef
binlyzhuo <binlyzhuo@outlook.com>
2017-01-12 20:31:37 +0800

    ~

--- Chpt13/Program.cs
+++ Chpt13/Program.cs
@@ -10,6 +10,13 @@ namespace Chpt13
     {
         static void Main(string[] args)
         {
+            Dump(x:3,y:2,z:1);
+            Console.ReadLine();
+        }
+
+        static void Dump(int x, int y, int z)
+        {
+            Console.WriteLine("x={0},y={1},z={2}",x,y,z);
         }
     }
 }
