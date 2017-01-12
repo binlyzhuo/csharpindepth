f242f434fdf01c111278f7bb1814602dc1e5eafd
binlyzhuo <binlyzhuo@outlook.com>
2017-01-12 20:50:15 +0800

    chpt15.AsyncForm

---
+++ Chpt15.AsyncForm/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chpt15.AsyncForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //DisplayLength();
+            label1.Text = "fetching...";
+            using (HttpClient client = new HttpClient())
+            {
+                string txt = await client.GetStringAsync("http://www.baidu.com");
+                label1.Text = txt.Length.ToString();
+            }
+        }
+
+        async void DisplayLength()
+        {
+            label1.Text = "fetching...";
+            using (HttpClient client = new HttpClient())
+            {
+                string txt = await client.GetStringAsync("http://www.baidu.com");
+                label1.Text = txt.Length.ToString();
+            }
+        }
+    }
+}
